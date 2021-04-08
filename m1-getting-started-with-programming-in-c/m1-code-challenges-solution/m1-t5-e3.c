7214fbbbe310b656509a2cb054135b2addf0712a
kl36 <82146334+kl36@users.noreply.github.com>
2021-04-08 14:28:50 +0530

    Update m1-t5-e3.c

--- m1-getting-started-with-programming-in-c/m1-code-challenges-solution/m1-t5-e3.c
+++ m1-getting-started-with-programming-in-c/m1-code-challenges-solution/m1-t5-e3.c
@@ -2,8 +2,8 @@
 
     int main() {
 
-        puts("I am Rahul.");
-        puts("I am 20 years old.");
+        puts("I am Lakshmi Kuna.");
+        puts("I am 24 years old.");
 
         return 0;
     }
